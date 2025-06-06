60bdc9a5dd3c5ac6ca680cc5bef2bfba37d29c0b
rohit <rohit@.com>
2025-06-06 17:21:24 +0000

    main.py modified

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.pp = Flask(__name__)
 
 @app.route("/")
 def home():
-    model = {"title": "Hello! Welcome to the GCP Deployed App"}
+    logging.info("Home page accessed from IP: %s", request.remote_addr)
+    return render_template("index.html")
+    model = {"title": "Hello! Welcome to the GCP Deployed Appp"}
     return render_template("index.html", model=model)
 
 if __name__ == "__main__":
+    logging.info("Starting Flask app on port 8080 . . .")
     app.run(host="0.0.0.0", port=8080)
